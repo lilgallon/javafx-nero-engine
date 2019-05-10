11f26fc7c33d249403e3429c07f66b535bc5a554
nero <swellchaser33@gmail.com>
2019-05-10 15:11:49 +0200

    mouse(feat): add mouse entered/exited & pressed/released

--- src/main/java/io/github/n3roo/events/mouse/Mouse.java
+++ src/main/java/io/github/n3roo/events/mouse/Mouse.java
@@ -8,6 +8,12 @@ public class Mouse {
     // y position of the mouse in the scene
     private static double y;
 
+    // true whenever the mouse is in the scene (useful only if we use multiple scenes)
+    private static boolean in = false;
+
+    // true whenever the mouse is being pressed
+    private static boolean pressed = false;
+
     /**
      * It updates the mouse position by overriding its current position.
      * @param x the new x position on the scene,
@@ -19,16 +25,46 @@ public class Mouse {
     }
 
     /**
+     * It updates whenever the mouse is in the scene or not.
+     * @param isIn true means that the mouse is in the scene.
+     */
+    static void isIn(boolean isIn){
+        Mouse.in = true;
+    }
+
+    /**
+     * It updates whenever the mouse is pressed or not.
+     * @param isPressed true means that the mouse is being pressed.
+     */
+    static void isPressed(boolean isPressed){
+        Mouse.pressed = isPressed;
+    }
+
+    /**
      * @return x position of the mouse in the scene.
      */
     public static double getX(){
-        return x;
+        return Mouse.x;
     }
 
     /**
      * @return y position of the mouse in the scene.
      */
     public static double getY(){
-        return y;
+        return Mouse.y;
+    }
+
+    /**
+     * @return true whenever the mouse is in the scene.
+     */
+    public static boolean isIn(){
+        return Mouse.in;
+    }
+
+    /**
+     * @return true whenever the mouse is being pressed.
+     */
+    public static boolean isPressed(){
+        return Mouse.pressed;
     }
 }
--- src/main/java/io/github/n3roo/events/mouse/MouseEvent.java
+++ src/main/java/io/github/n3roo/events/mouse/MouseEvent.java
@@ -7,5 +7,19 @@ public class MouseEvent {
     public static void init(Scene scene){
         // This event is fired whenever the mouse is moved (the scene must be focused)
         scene.setOnMouseMoved(event -> Mouse.updatePosition(event.getSceneX(), event.getSceneY()));
+
+        // This event is fired whenever the mouse enters the scene
+        scene.setOnMouseEntered(event -> Mouse.isIn(true));
+
+        // This event is fired whenever the mouse leaves the scene
+        scene.setOnMouseExited(event -> Mouse.isIn(false));
+
+        // This event is fired whenever the mouse is pressed
+        scene.setOnMousePressed(event -> Mouse.isPressed(true));
+
+        // This event is fired whenever the mouse is pressed
+        scene.setOnMouseReleased(event -> Mouse.isPressed(false));
+
+        // TODO: implement all the drag methods
     }
 }
