bdee2e7e932d30b2b32b1ac2c64d5a39926e0595
nero <swellchaser33@gmail.com>
2019-05-10 17:23:46 +0200

    events(fix): the engine now exits properly

--- src/main/java/io/github/n3roo/events/EventManager.java
+++ src/main/java/io/github/n3roo/events/EventManager.java
@@ -2,6 +2,7 @@ package io.github.n3roo.events;
 
 import io.github.n3roo.events.keyboard.KeyEvent;
 import io.github.n3roo.events.mouse.MouseEvent;
+import javafx.application.Platform;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -14,6 +15,12 @@ public class EventManager {
     public static void init(Stage stage){
         KeyEvent.init(stage.getScene());
         MouseEvent.init(stage.getScene());
+
+        // Close events
+        addOnEngineClosingListener(() -> {
+            Platform.exit();
+            System.exit(0);
+        });
         stage.setOnCloseRequest(windowEvent -> listeners.forEach(OnEngineClosing::performAction));
     }
 
