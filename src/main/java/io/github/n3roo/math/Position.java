5d39e6f2ed4ad4fca8e797509f4373723baf4ec6
nero <swellchaser33@gmail.com>
2019-05-11 19:50:33 +0200

    math: Position implementation

---
+++ src/main/java/io/github/n3roo/math/Position.java
@@ -0,0 +1,44 @@
+package io.github.n3roo.math;
+
+public class Position {
+
+    /**
+     * Horizontal position
+     */
+    public double x;
+
+    /**
+     * Vertical position
+     */
+    public double y;
+
+    /**
+     * It creates a position with the defined (x; y).
+     * @param x horizontal position,
+     * @param y vertical position.
+     */
+    public Position(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * It creates a position at (0; 0).
+     */
+    public Position(){
+        this.x = 0;
+        this.y = 0;
+    }
+
+    /**
+     * Moves the position with the given deltas.
+     * @param dx x delta,
+     * @param dy y delta.
+     * @return this instance.
+     */
+    public Position move(double dx, double dy){
+        this.x += dx;
+        this.y += dy;
+        return this;
+    }
+}
