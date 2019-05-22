ceaadfb5fd84df0354c4a2dbab8f8715dc1772d8
nero <swellchaser33@gmail.com>
2019-05-22 14:18:21 +0200

    math: rigid body implementation (will be used for polygon collisions)

---
+++ src/main/java/io/github/n3roo/math/RigidBody.java
@@ -0,0 +1,40 @@
+package io.github.n3roo.math;
+
+import javafx.scene.shape.Polygon;
+
+public class RigidBody {
+
+    /**
+     * Shape of this rigid body.
+     */
+    private Polygon polygon;
+
+    /**
+     * Mass of this rigid body.
+     */
+    private float mass;
+
+    /**
+     * It creates a collision box. It is a square, with the (x, y) position being the top left corner.
+     * @param polygon the collision polygon,
+     * @param mass the mass of the rigid body. (value < 0 means an infinite mass)
+     */
+    public RigidBody(Polygon polygon, float mass){
+        this.polygon = polygon;
+        this.mass = mass;
+    }
+
+    /**
+     * @return the shape of this rigid body.
+     */
+    public Polygon getPolygon(){
+        return this.polygon;
+    }
+
+    /**
+     * @return the mass of this rigid body.
+     */
+    public float getMass(){
+        return this.mass;
+    }
+}
