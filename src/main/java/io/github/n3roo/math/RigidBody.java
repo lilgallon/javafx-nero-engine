f927b43ff2ee6a00c851f06bfc9284f58996ff9c
nero <swellchaser33@gmail.com>
2019-05-25 10:24:59 +0200

    physics(refacto): rename "polygon" to "hitbox"

--- src/main/java/io/github/n3roo/math/RigidBody.java
+++ src/main/java/io/github/n3roo/math/RigidBody.java
@@ -7,7 +7,7 @@ public class RigidBody {
     /**
      * Shape of this rigid body.
      */
-    private Polygon polygon;
+    private Polygon hitbox;
 
     /**
      * Mass of this rigid body.
@@ -16,19 +16,19 @@ public class RigidBody {
 
     /**
      * It creates a collision box. It is a square, with the (x, y) position being the top left corner.
-     * @param polygon the collision polygon,
+     * @param hitbox the collision polygon,
      * @param mass the mass of the rigid body. (value < 0 means an infinite mass)
      */
-    public RigidBody(Polygon polygon, float mass){
-        this.polygon = polygon;
+    public RigidBody(Polygon hitbox, float mass){
+        this.hitbox = hitbox;
         this.mass = mass;
     }
 
     /**
      * @return the shape of this rigid body.
      */
-    public Polygon getPolygon(){
-        return this.polygon;
+    public Polygon getHitbox(){
+        return this.hitbox;
     }
 
     /**
