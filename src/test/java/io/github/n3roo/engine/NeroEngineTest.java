babb2327182e87bf9a238fb33f97aecd50abcfc1
nero <swellchaser33@gmail.com>
2019-05-27 14:16:06 +0200

    tests(NeroEngine): add NeroEngine tests

--- src/test/java/io/github/n3roo/engine/NeroEngineTest.java
+++ src/test/java/io/github/n3roo/engine/NeroEngineTest.java
@@ -1,12 +1,29 @@
 package io.github.n3roo.engine;
 
+import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(ApplicationExtension.class)
 class NeroEngineTest {
 
+    private Exception exception = null;
+
+    @Start
+    void start(Stage stage) {
+        try{
+            NeroEngine.init(stage);
+        } catch (Exception e) {
+            exception = e;
+        }
+    }
+
     @Test
-    void init() {
+    void init(){
+        assertNull(exception);
     }
 }
