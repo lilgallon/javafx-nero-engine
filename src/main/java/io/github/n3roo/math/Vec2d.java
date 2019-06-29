c4db84da52c444bc6b69b9dcc936f9ecf594f914
nero <swellchaser33@gmail.com>
2019-06-29 16:09:37 +0200

    math: add vec2d class

--- src/main/java/io/github/n3roo/math/Vec2d.java
+++ src/main/java/io/github/n3roo/math/Vec2d.java
@@ -5,8 +5,31 @@ public class Vec2d {
     public double x;
     public double y;
 
-    public Vec2d(double x, double y){
+    public Vec2d() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Vec2d(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    public Vec2d add(double value) {
+        this.x += value;
+        this.y += value;
+        return this;
+    }
+
+    public Vec2d multiply(double value) {
+        this.x *= value;
+        this.y *= value;
+        return this;
+    }
+
+    public Vec2d add(double dx, double dy) {
+        this.x += dx;
+        this.y += dy;
+        return this;
     }
 }
