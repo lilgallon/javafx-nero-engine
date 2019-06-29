b7960a08fe13d5a4eb05a522b5d02ab9211d20a2
nero <swellchaser33@gmail.com>
2019-06-29 14:21:13 +0200

    physics(shapes): improve rectangle constructor to be easier to use

--- src/main/java/io/github/n3roo/math/body/shapes/Rectangle.java
+++ src/main/java/io/github/n3roo/math/body/shapes/Rectangle.java
@@ -10,11 +10,27 @@ public class Rectangle extends Polygon {
      * It creates a rectangle.
      *
      * @param position the position of the first point in the world,
+     * @param width the width of the rectangle,
+     * @param height the height of the rectangle,
      * @param rotation the shape rotation (from the position),
-     * @param model    an array of points representing the rectangle (4 points).
+     * @param originInCenter true means that the position given is the center. Otherwise, it corresponds to the top left
+     *                       corner.
      */
-    public Rectangle(Position position, double rotation, ArrayList<Position> model) {
-        super(position, rotation, model);
-        if(model.size() != 4) throw new IllegalArgumentException("Model strictly requires 4 points for a rectangle.");
+    public Rectangle(Position position, double width, double height, double rotation, boolean originInCenter) {
+        super(position, rotation, new ArrayList<>());
+
+        ArrayList<Position> model = new ArrayList<>();
+        if(originInCenter) {
+            model.add(new Position(- width / 2, - height / 2));
+            model.add(new Position(width / 2, - height / 2));
+            model.add(new Position(width / 2, height / 2));
+            model.add(new Position(- width / 2, height / 2));
+        } else {
+            model.add(new Position(0, 0));
+            model.add(new Position(width, 0));
+            model.add(new Position(width, height));
+            model.add(new Position(0, height));
+        }
+        setModel(model);
     }
 }
